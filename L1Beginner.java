078e21394b9164c9cd27a1cbb429de3e17d581df
Rayanna <rayanna.xu1@gmail.com>
2025-04-05 23:01:49 -0400

    Add files via upload

---
+++ L1Beginner.java
@@ -0,0 +1,22 @@
+package l1;
+// java is an object-oriented, every related code is a class.
+// to start: create a project -> (right click the project) create a class.
+public class L1Beginner {
+	// must have the following line to run
+	// to have this line, check the box when creating the class
+	public static void main(String[] args) {
+		// the code start running here to the bottom
+		// @@@ printing
+		System.out.print("I love pizza"); // printing in the console
+		System.out.print("It's really good"); // follow the previous!
+		System.out.println("I love pasta\n"); // How to newline: println OR \n. (can overlay)
+		System.out.println("    It's really\t good too"); // \t  not working don't know why
+		System.out.print("\"I love pizza\""); // display ""
+		/*
+		 * for multiple line comments
+		 * this is an example
+		 */
+
+	}
+
+}
