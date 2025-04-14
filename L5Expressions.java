dcffbad656a47bb801fcc09daee04b14c473ee17
Rayanna <rayanna.xu1@gmail.com>
2025-04-14 16:13:25 -0400

    L5

---
+++ L5Expressions.java
@@ -0,0 +1,26 @@
+package courseNote;
+
+public class L5Expressions {
+	// expression
+	public static void main(String[] args) {
+		// expression = operands & operators
+		// operands = values, variables, numbers, quantity
+		// operators = + - * / %
+		int friends = 10;
+		friends = friends + 1; // expression. % gets the remainder
+		System.out.println(friends);
+		friends ++; // short cut of increase by 1. "--" work the same way
+		System.out.println(friends);
+		int number = 10;
+		number = number / 3;
+		System.out.println(number); // in this way, number will be 3 since number is a int type
+		// to store the decimal parts
+		double decimal = 10;
+		decimal = decimal / 3;
+		System.out.println(decimal); // print 3.333...5
+		
+		
+
+	}
+
+}
