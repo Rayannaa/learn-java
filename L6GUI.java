b1a9a7feec7ae2d489ad5e779b0c506d5d628b2d
Rayanna <rayanna.xu1@gmail.com>
2025-05-10 05:03:20 -0400

    L6GUI
    Long time no see:) Too busy in the final weeks, and I just finished my long trip back to my hometown. Congratulations to myself on completing my first year at McGill.

--- L6GUI.java
+++ L6GUI.java
@@ -1,12 +1,28 @@
-import javax.swing.JOptionPane;
+package learnJavaAgain;
 
-package 又得重新来了;
+/*
+ * Long time no see:)
+ * This course is about using dialogue box to input and output
+ * Tips: if the inport Javax.swing does not work, type "requires java.desktop;" in the module-info.java file
+ */
+import javax.swing.JOptionPane; // import JOptionPane class
 
 public class L6GUI {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// create a input dialogue box
+		String name = JOptionPane.showInputDialog("Enter your name");
+		// create a display dialogue box
+		JOptionPane.showMessageDialog(null, "Hello " + name);
+		
+		// input dialogue box resturns a str automatically
+		int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age")); //convert str to int
+		JOptionPane.showMessageDialog(null, "You are " + age + " years old");
+		
+		double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+		JOptionPane.showMessageDialog(null, "You are " + height + " cm tall");
+		
+		
 	}
 
 }
