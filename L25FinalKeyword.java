72287f1ac2c6cd4bba9f964ce7193199805f8533
Rayanna <rayanna.xu1@gmail.com>
2025-06-23 05:17:12 -0400

    L25

---
+++ L25FinalKeyword.java
@@ -0,0 +1,15 @@
+package l23;
+
+
+// final keyword: can't be changed or updated later
+public class L25FinalKeyword {
+
+	public static void main(String[] args) {
+		// for constant
+		final double PI = 3.14159; 
+		// pi = 4; this is not allowed
+		System.out.println(PI);
+
+	}
+
+}
